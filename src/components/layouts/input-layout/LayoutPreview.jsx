3c44868cfeb3facea280881804da81367166210e
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-08-17 19:15:02 +0530

    Stabilize neutral A4 CV rendering

--- src/components/layouts/input-layout/LayoutPreview.jsx
+++ src/components/layouts/input-layout/LayoutPreview.jsx
@@ -1,24 +1,22 @@
 
 import { LayoutWrapperWithBorder, ResumesWrapperDiv } from "../../elements/resumeWrapper";
 import { H3 } from "../../CustomComponents"
-import LayoutByType from "../LayoutByType";
-import { useParams } from "react-router-dom";
 import { useLayout } from "../../../provider/layoutProvider";
 import Loading from "../../Loading";
+import StableResumeDocument from "../../StableResumeDocument";
 
 
 
 const LayoutPreview = () => {
-    const { layout_type, layout_id } = useParams()
-    const { isDetailsUpdating, resumeTheme } = useLayout()
+    const { isDetailsUpdating } = useLayout()
     return (
         <LayoutWrapperWithBorder className="preview-card" padding="0">
             <div className="preview-heading"><div><span>LIVE DOCUMENT</span><H3>Resume preview</H3></div><small>A4 · ATS-friendly</small></div>
             <div className="preview-stage">
-                <ResumesWrapperDiv className="preview-canvas wrapper-div" data-cv-theme={resumeTheme}>
+                <ResumesWrapperDiv className="preview-canvas wrapper-div">
                     {isDetailsUpdating ? <Loading message="updating details" />
                         :
-                        <LayoutByType key={`${layout_type}-${layout_id}`}></LayoutByType>}
+                        <StableResumeDocument />}
                 </ResumesWrapperDiv>
             </div>
         </LayoutWrapperWithBorder>
