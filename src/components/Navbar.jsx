fba8e6c1e48850896d040bdbdf403a8129405db6
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-08-18 10:43:14 +0530

    Fix account logout menu and upgrade login experience

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import UserCard from "./UserCard";
 import ThemeToggler from "./ThemeToggler";
 import { StyledNavLink } from "./CustomComponents";
 import { useTheme } from "styled-components";
+import "../css/account-menu.css";
 ;
 
 
