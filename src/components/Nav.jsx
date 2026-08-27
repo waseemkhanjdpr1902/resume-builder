223c46f8c2e17e342122413a301d5154744ac83f
waseemkhanjdpr1902 <waseemkhanjdpr@gmail.com>
2026-08-27 14:14:54 +0530

    Add Academy to navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,6 +10,7 @@ const Nav = () => {
         <li><StyledNavLink to="/uae-healthcare-jobs">UAE Jobs</StyledNavLink></li>
         <li><StyledNavLink to="/healthcare-cv-examples">Free Resources</StyledNavLink></li>
         <li><StyledNavLink to="/healthcare-interview-coach">Interview Coach</StyledNavLink></li>
+        <li><StyledNavLink to="/academy">Academy</StyledNavLink></li>
         <li><StyledNavLink to="/licensing-exam-prep">Exam Prep</StyledNavLink></li>
         <li><StyledNavLink to="/credential-readiness">Country Readiness</StyledNavLink></li>
         <li><StyledNavLink to="/pricing">Pricing</StyledNavLink></li>
